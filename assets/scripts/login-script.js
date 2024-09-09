7f1d536fbcdb58eb05d87d3f9ddf4abe324cc95b
abrory <63485291+wahyuabrory@users.noreply.github.com>
2024-09-09 15:41:44 +0700

    change comment to match the JSDoc Format

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,60 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+/** @module Login-Script */
+/** 
+ * Creates a variable `loginFormElement` for the form display.
+ * @constant {HTMLElement}
  */
-
 const loginFormElement = document.querySelector('#loginForm');
+
+/** 
+ * Creates a variable `inputEmailElement` for the email input field.
+ * @constant {HTMLElement}
+ */
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/** 
+ * Creates a variable `inputPasswordElement` for the password input field.
+ * @constant {HTMLElement}
+ */
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/** 
+ * Creates a variable `expectedEmail` to store the temporary email information.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
+
+/** 
+ * Creates a variable `expectedPassword` to store the temporary password information.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
 
+/* Comment: Adds a click action to the button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
-
+  
+  /** 
+   * Creates a variable `email` to store the email value obtained when the button is pressed.
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
+  
+  /** 
+   * Creates a variable `password` to store the password value obtained when the button is pressed.
+   * @constant {string}
+   */
   const password = inputPasswordElement.value;
+ 
+  /* Comment: Checks if the email and password values match the stored values. */
   if (email == expectedEmail && password == expectedPassword) {
+ 
+    /* Comment: If they match, the program will navigate to the home page. */
     goToHome();
+ 
   } else {
+ 
+    /* Comment: If they do not match, it will display a message indicating incorrect input. */
     showPopUp();
+ 
   }
 });
